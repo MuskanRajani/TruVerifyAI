e452b282544547e592d090b9d93aefc8f8c7ba55
Muskan Rajani <85781792+MuskanRajani@users.noreply.github.com>
2025-12-13 00:09:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/TruVerifyAI/", // same as your repo name
+  base: "/TruVerifyAI/",   // repo name
+  build: {
+    outDir: "docs",        // output folder GitHub Pages uses
+  },
 });
