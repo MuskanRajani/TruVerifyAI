1f4e205cc26f92f213d1f527f1ffbf8107e2b779
Muskan Rajani <85781792+MuskanRajani@users.noreply.github.com>
2025-12-12 23:36:55 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/TruVerifyAI/",   // <-- REQUIRED FOR GITHUB PAGES
+  base: "/TruVerifyAI/", // same as your repo name
 });
